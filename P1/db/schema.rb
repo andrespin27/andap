d5083e78045f9ca5101d1858965dcbf2a2fb136d
apineda <andrespin27@gmail.com>
2018-08-28 21:21:43 -0500

    clase_20180828_

--- P1/db/schema.rb
+++ P1/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180815015000) do
+ActiveRecord::Schema.define(version: 20180829015532) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "picture"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
